08d558b42da1fcdc21a13ee3e4bff09c7229ba5d
Jagjeet Singh <129967275+jagjeet-singh04@users.noreply.github.com>
2025-08-26 04:07:05 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-// import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [
-    react(),
-    
-  ],
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      // Force using standard JS version
+      useNative: false
+    }
+  }
 })
